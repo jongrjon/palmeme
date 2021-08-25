7850d1bfff786004962a98711e65cc6ed6303edc
Jón Helgi Kjartansson <jongrjon@gmail.com>
2021-08-25 23:17:36 +0000

    ad

--- main.js
+++ main.js
@@ -1,3 +1,66 @@
+<<<<<<< Updated upstream
+=======
+//Fetching Video element by ID
+var video = document.getElementById('video');
+
+// Basic settings for the video to get from Webcam  
+const constraints = {  
+    audio: false,  
+    video: {  
+        width: 200, height: 200  
+    }  
+};  
+
+// This condition will ask permission to user for Webcam access  
+if (navigator.mediaDevices.getUserMedia) {  
+    navigator.mediaDevices.getUserMedia(constraints)  
+        .then(function (stream) {  
+            video.srcObject = stream;  
+        })  
+        .catch(function (err0r) {  
+            console.log("Something went wrong!");  
+        });  
+}  
+
+function stop(e) {  
+    var stream = video.srcObject;  
+    var tracks = stream.getTracks();  
+
+    for (var i = 0; i < tracks.length; i++) {  
+        var track = tracks[i];  
+        track.stop();  
+    }  
+    video.srcObject = null;  
+}
+
+//VIDEO CANVAS
+
+//Fetching Video Canvas
+var v = document.getElementById("vcanv");
+var vtx = v.getContext("2d");
+
+//Creating snapped image in code
+var snap = new Image;
+snap.src = "";
+
+//Drawing snapped image to Video Canvas
+picsnap = function(){
+	vtx.clearRect(0, 0, c.width, c.height);
+	vtx.restore();
+	vtx.drawImage(video, 312, 215);
+	vtx.drawImage(background,0,0);
+	snap = c.toDataURL("image/jpg");
+	snap.crossOrigin="anonymous";
+	change(lastval);
+	$("#editPButton").toggle();
+	$("#snapButton").toggle();
+
+}
+
+//PICTURE CANVAS
+
+//Fetching Picture Canvas
+>>>>>>> Stashed changes
 var c = document.getElementById("pcanv");
 var ctx = c.getContext("2d");
 
